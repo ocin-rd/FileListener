b7e826433a8c457f6ed89c74cca87abb3f31c1c4
ocin-rd <54357136+ocin-rd@users.noreply.github.com>
2020-08-22 21:41:11 +0100

    Implementation of FileListener

--- FileListener.kt
+++ FileListener.kt
@@ -1,11 +1,15 @@
 import java.io.File
+import java.time.LocalDate
 
+/* This class takes a format definition and finds the matching data field.
+   The result is a list of integers which are indices into the data field set */
 class Classifier(val format: String) {
       val data_fields = setOf<String>("labname", "patientnumber", "firstname", "lastname", "dob", "referencenumber")
       var content = mutableSetOf<Int>()
       init {
       	  var data: List<String>  = format.split("_")
 	  for (d in data) {
+	      /* The data fields are case insensitive */
 	      val data_field = data_fields.find {it.equals(d, true)}
 	      if (data_field != null) {
 	      	 val index = data_fields.indexOf(data_field)
@@ -20,6 +24,8 @@ class Classifier(val format: String) {
       }
 }
 
+/* This class produces a number of random text files in a given directory.
+   The random data is defined based on the data fields in the Classifier. */
 class FileFactory(val classifier: Classifier, val directory: String) {
       val random_data: Map<String, Set<String>> =
       	  mapOf(classifier.data_fields.elementAt(0) to setOf("LabA", "LabB", "LabC", "LabD"),
@@ -29,13 +35,13 @@ class FileFactory(val classifier: Classifier, val directory: String) {
 	  classifier.data_fields.elementAt(4) to setOf("19701201", "19520816", "20010509", "19900227"),
 	  classifier.data_fields.elementAt(5) to setOf("5487", "14", "797", "687"))
       init {
-      	  val dir = File(directory)
-	  if (!dir.exists()) {
+      	  val dir = File(directory) 
+      	  if (!dir.exists()) {
 	     dir.mkdirs()
 	  }
       }
       fun create_files(n: Int) {
-      	  println("Creating ${n} files")
+      	  println("Creating ${n} random files in directory ${directory}/\n")
 	  for (i in 1..n) {
 	      var filename: String = ""
 	      for (data in classifier.content) {
@@ -49,12 +55,95 @@ class FileFactory(val classifier: Classifier, val directory: String) {
       }
 }
 
+/* This class is used to scan all files in the source directory, to check whether the filenames
+   match the given format and if they do extract the metadata and archive the file. */
+class FileListener(val classifier: Classifier, val source_dir: String) {
+      val source = File(source_dir)
+      /* Archive needs to be initialised, but gets overwritten once files actually
+         get archived */
+      var archive = File("tmp")
+      fun process() {
+      	  val walker = source.walk()
+	  val file_iterator = walker.iterator()
+	  file_iterator.forEach {
+	      if (it.isFile()) {
+	      	 val is_valid = extract_metadata(it)
+		 if (is_valid) {
+	      	    archive_file(it)
+		    println()
+		 }
+		 else {
+		    println("Skipping file ${it} because it does not match the given format.")
+		 }
+	      }
+	  }
+      }
+      fun extract_metadata(file: File): Boolean {
+      	  var is_valid = false
+	  var filename = file.toString()
+	  /* Store metadata in a map */
+	  var metadata = mutableMapOf<String, String>()
+	  /* Remove directory and file extension */
+	  filename = filename.replaceBeforeLast("/", "")
+	  filename = filename.drop(1)
+	  filename = filename.replaceAfterLast(".", "")
+	  filename = filename.dropLast(1)
+	  
+	  val data: List<String> = filename.split("_")
+	  if (data.size == classifier.content.size) {
+	     for (i in 0..data.size-1) {
+	     	 metadata.put(classifier.data_fields.elementAt(classifier.content.elementAt(i)), data.elementAt(i))
+	     }
+	     println(metadata)
+	     is_valid = true
+	  }
+	  return is_valid
+      }
+      fun archive_file(file: File) {
+      	  make_archive()
+	  
+	  var filename = file.toString()
+	  filename = filename.replaceBefore("/", "")
+	  filename = filename.drop(1)
+	  
+	  var target = file.toString()
+	  target = target.replace(source_dir, archive.toString())
+	  if (!File(target).exists()) {
+	     file.copyTo(File(target))
+	     file.delete()
+	     println("Archived file ${filename} in directory ${archive.toString()}/")
+	  }
+	  else {
+	     println("File ${filename} already exists in directory ${archive.toString()}/")
+	  }
+      }
+      fun make_archive() {
+      	  val date = LocalDate.now()
+	  archive = File(date.toString())
+	  if (!archive.exists()) {
+	     archive.mkdirs()
+	  }
+      }
+}
+
 fun main(args: Array<String>) {
     if (args.size != 2) {
-       println("Please provide an input directory and a single string representing the filename format")
+       println("Please provide a source directory and a single string representing the filename format")
        return
     }
-    val classifier = Classifier(args[1])
-    val filefactory = FileFactory(classifier, args[0].replace("/", ""))
-    filefactory.create_files(4)
+    var source_directory = args[0]
+    val filename_format = args[1]
+    
+    val classifier = Classifier(filename_format)
+    
+    /* Remove any trailing slash from the directory path */
+    source_directory = source_directory.replace("/", "")
+    val dir = File(source_directory)
+    if (!dir.exists()) {
+       val filefactory = FileFactory(classifier, source_directory)
+       filefactory.create_files(4)
+    }
+
+    val filelistener = FileListener(classifier, source_directory)
+    filelistener.process()
 }
